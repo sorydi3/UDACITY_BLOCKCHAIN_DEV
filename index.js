53f79546470f0089f939df79649a79a0db45584f
ibradi3 <ibrahimasory22@hotmail.com>
2022-05-17 17:59:58 +0200

    about block and java promises

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-var sha256 =  require("crypto-js/sha256");
-const data1 = "Bllochaicn 1";
-const dataObject = {
-    id:1,
-    body: "with Objects works too",
-    time: new Date().getTime().toString().slice(0,-3)
-}
+const Blockclass = require('./block.js')
 
-function generateHash(obj) {
-    return sha256(JSON.stringify(obj));
-}
 
-console.log(`SHA256 Hash: ${generateHash(data1)}`);
-console.log(`SHA256 Hash: ${generateHash("HELLOW WORLD")}`);
-console.log(`SHA256 Hash: ${generateHash(dataObject)}`);
-console.log(`SHA256 Hash: ${generateHash("MY NAME IS IBRAHIMA SORY")}`);
+// const data1 = "Blockchain Rock!";
+// const dataObject = {
+//     id:1,
+//     body: "with Objects works too",
+//     time: new Date().getTime().toString().slice(0,-3)
+// }
+
+// function generateHash(obj) {
+//     return sha256(JSON.stringify(obj));
+// }
+
+// console.log(`SHA256 Hash: ${generateHash(data1)}`);
+// console.log(`SHA256 Hash: ${generateHash("HELLOW WORLD")}`);
+// console.log(`SHA256 Hash: ${generateHash(dataObject)}`);
+// console.log(`SHA256 Hash: ${generateHash("MY NAME IS IBRAHIMA SORY")}`);
+
+//2fad68ec976395e50513979ed65fa27b4a959741838b1714e4ef910afa56d6b1
+//creating the block
+const block = new Blockclass.Block("Test Block");
+
+//generating the block hash
+block.generateHash().then((result)=>{
+ console.log(`Block hash: ${result.hash}`)
+ console.log(`Block hash: ${JSON.stringify(result)}`)
+}).catch((error) => {console.log(error)});
+
